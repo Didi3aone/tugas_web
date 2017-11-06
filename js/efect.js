62a1f7b309bc039ee2cd28d26e28f9f71d76ce5f
DIdi31 <DIdithreeaone@gmail.com>
2017-11-06 18:14:00 +0700

    update cuy

--- js/efect.js
+++ js/efect.js
@@ -1,6 +1,6 @@
 var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 var month = ["Januari", "Februari", "maret","april","mei","juni","juli","agustus","september","oktober","November","desember"];
-var date = ["0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31"]; 
+var date = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31"]; 
 var now = new Date();
 
 // document.getElementById("demo").innerHTML = Date();
